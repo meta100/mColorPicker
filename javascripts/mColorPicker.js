13f05812121cc3d129e38b693679270bfddaccc0
William Shostak <wshostak@cerego.com>
2011-11-15 03:48:04 +0900

    Small css typo fix

--- javascripts/mColorPicker.js
+++ javascripts/mColorPicker.js
@@ -183,11 +183,11 @@
     }).insertAfter($t)
     
     $(img).attr({
-      'src': $o.imageFolder + 'color.png',
-      'align': 'top'
+      'src': $o.imageFolder + 'color.png'
     }).css({
       'border': 0,
-      'margin': '1px 0 0 3px'
+      'margin': '0 0 0 3px',
+      'vertical-align': 'text-bottom'
     }).appendTo($trigger);
 
     $c.append($t);
